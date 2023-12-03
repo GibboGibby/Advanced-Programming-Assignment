162bf6b667fbc52eee9ea525b190d916ad42c912
GibboGibby <66483732+GibboGibby@users.noreply.github.com>
2023-12-03 11:52:19 +0000

    Changed some stuff to test it on my laptop and it works! just updating the repo to avoid any issues

--- ApplicationClient/Client.cpp
+++ ApplicationClient/Client.cpp
@@ -30,7 +30,7 @@ int main()
 
 	server.sin_family = AF_INET;
 	server.sin_port = htons(PORT);
-	server.sin_addr.s_addr = inet_addr("127.0.0.1");
+	server.sin_addr.s_addr = inet_addr(SERVER);
 
 	while (true)
 	{
